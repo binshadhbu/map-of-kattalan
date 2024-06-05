507b3a39d0dc02ffbaebf28440843a3c4a8a285b
binshadhbu <binshadh_b210517cs@nitc.ac.in>
2024-06-05 20:50:46 +0530

    restart from 1:53:40

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Layout from './components/Layout'
 import RegisterPage from './pages/RegisterPage'
 import axios from 'axios'
 import { UserContextProvider } from './context/UserContext'
+import AccountPage from './pages/AccountPage'
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/" element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<AccountPage/>} />
         </Route>
       </Routes>
     </UserContextProvider>
---
+++ client/src/pages/AccountPage.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../context/UserContext';
+import { Navigate } from 'react-router-dom';
+
+function AccountPage() {
+    const {ready,user } =useContext(UserContext);
+    if(!ready) return 'Loading...';
+
+    if(ready &&!user){
+      return <Navigate to={'/login'}/>
+    }
+
+  return (
+    <div>AccountPage {user?.name}</div>
+  )
+}
+
+export default AccountPage
